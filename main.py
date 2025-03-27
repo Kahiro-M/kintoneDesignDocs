a5f4ad120137709a134c6da97c21fb514e3ee9f1
Kahiro-M <0204.kahiro@gmail.com>
2025-03-27 13:26:24 +0900

    appList.csvに記載されているアプリの情報を取得する処理追加

--- main.py
+++ main.py
@@ -6,6 +6,30 @@ config = loadIni.readConfig('config.ini')
 
 appList = kintone.loadCsv('appList.csv')
 apiToken = kintone.getAllApiToken(appList)
-curl.getFieldsInfo(subdomain,id,apiToken,json_path)
-kintone.createDocs(json_path,csv_main_path,csv_lookup_path, csv_reference_path)
+for appDict in appList:
+    exeFlg = True
+    json_path = ''
+    csv_main_path="kintone_fields.csv"
+    csv_lookup_path="kintone_lookup.csv"
+    csv_reference_path="kintone_reference_table.csv"
+    if(appDict.get('id')):
+        json_path = appDict.get('id')+'_'
+        csv_main_path = appDict.get('id')+'_'
+        csv_lookup_path = appDict.get('id')+'_'
+        csv_reference_path = appDict.get('id')+'_'
+    else:
+        exeFlg = False
+    if(appDict.get('name')):
+        json_path += appDict.get('name')+'_appDesignInfo.json'
+        csv_main_path += appDict.get('name')+'_kintone_fields.csv'
+        csv_lookup_path += appDict.get('name')+'_kintone_lookup.csv'
+        csv_reference_path += appDict.get('name')+'_kintone_reference_table.csv'
 
+    else:
+        exeFlg = False
+    if(appDict.get('api')==None):
+        exeFlg = False
+
+    if(exeFlg):
+        curl.getFieldsInfo(config['subdomain'],appDict.get('id'),apiToken,json_path)
+        kintone.createDocs(json_path,csv_main_path,csv_lookup_path,csv_reference_path)
